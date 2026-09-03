18cef29b2d25cb428e06e3d0e370004cae74f5a1
zolifaga-design <yeozolifagasamuel@gmail.com>
2026-09-04 01:01:35 +0200

    Add /api/files/list route

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: '256kb' }));
+app.use(express.urlencoded({ extended: false, limit: '256kb' }));
 app.use(express.static('.') );
 
 // ==========================
