f41745481da6ceafde213ad76d23364838a83e66
zolifaga-design <yeozolifagasamuel@gmail.com>
2026-09-22 21:29:11 +0200

    Add /api/files/list route

--- services/googleOAuthService.js
+++ services/googleOAuthService.js
@@ -36,8 +36,7 @@ function getAuthUrl(userId) {
     return oauth2Client.generateAuthUrl({
         access_type: "offline",
         scope: [
-            "https://www.googleapis.com/auth/drive",
-            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile"
         ],
